034c0b712b8f4c2a08ee6df13c369df770c014a4
BertelloG <giany1698@gmail.com>
2024-11-05 21:19:50 -0300

    actualizacion del repositorio 1

--- PP/JS/Productos.js
+++ PP/JS/Productos.js
@@ -124,3 +124,65 @@ export const velas = [
 }
 ];
 
+export const piedras = [
+    {
+        name: "Producto 1",
+        description: "Descripción del Producto 1",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Producto 2",
+        description: "Descripción del Producto 2",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Producto 3",
+        description: "Descripción del Producto 3",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Producto 4",
+        description: "Descripción del Producto 4",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Producto 5",
+        description: "Descripción del Producto 5",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Producto 6",
+        description: "Descripción del Producto 6",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+      name: "Producto 7",
+      description: "Descripción del Producto 1",
+      image: "https://via.placeholder.com/200"
+  },
+  {
+      name: "Producto 8",
+      description: "Descripción del Producto 2",
+      image: "https://via.placeholder.com/200"
+  },
+  {
+      name: "Producto 9",
+      description: "Descripción del Producto 3",
+      image: "https://via.placeholder.com/200"
+  },
+  {
+      name: "Producto 10",
+      description: "Descripción del Producto 4",
+      image: "https://via.placeholder.com/200"
+  },
+  {
+      name: "Producto 11",
+      description: "Descripción del Producto 5",
+      image: "https://via.placeholder.com/200"
+  },
+  {
+      name: "Producto 12",
+      description: "Descripción del Producto 6",
+      image: "https://via.placeholder.com/200"
+  }
+  ];
--- PP/JS/script.js
+++ PP/JS/script.js
@@ -32,7 +32,7 @@ function displaySahumerios() {
             <img src="${product.image}" class="card-img-top" alt="${product.name}">
             <div class="card-body">
             <h5 class="card-title">${product.name}</h5>
-            <p class"card-text">${product.description}</p>
+            <p class="card-text">${product.description}</p>
             </div>
         </div>
         `;
@@ -43,7 +43,7 @@ function displaySahumerios() {
 
 function scrollCards_S(direction) {
     const s_container = document.getElementById('sahumerios-container');
-    const cardWidth = s_container.children[0].offsetWidth +10;
+    const cardWidth = 2 * s_container.children[0].offsetWidth + 20;
 
     if (direction === 'next') {
         s_container.scrollBy({left: cardWidth, behavior: 'smooth'});
@@ -51,8 +51,8 @@ function scrollCards_S(direction) {
         s_container.scrollBy({left : -cardWidth, behavior: 'smooth'});
     }
 }
-document.getElementById('nextButton').addEventListener('click', () => scrollCards_S('next'));
-document.getElementById('prevButton').addEventListener('click', () => scrollCards_S('prev'));
+document.getElementById('s_nextButton').addEventListener('click', () => scrollCards_S('next'));
+document.getElementById('s_prevButton').addEventListener('click', () => scrollCards_S('prev'));
 
 displaySahumerios();
 //____________Velas_________________________________________________________________________________________________________________________________________
@@ -77,9 +77,9 @@ function displayVelas() {
   });
 }
 
-/*function scrollCards_V(direction) {
+function scrollCards_V(direction) {
   const v_container = document.getElementById('velas-container');
-  const cardWidth = v_container.children[0].offsetWidth + 10; 
+  const cardWidth = 2 * v_container.children[0].offsetWidth + 20; 
 
   if (direction === 'next') {
       v_container.scrollBy({ left: cardWidth, behavior: 'smooth' });
@@ -88,11 +88,46 @@ function displayVelas() {
   }
 }
 
-document.getElementById('nextButton').addEventListener('click', () => scrollCards_V('next'));
-document.getElementById('prevButton').addEventListener('click', () => scrollCards_V('prev'));
-*/
+document.getElementById('v_nextButton').addEventListener('click', () => scrollCards_V('next'));
+document.getElementById('v_prevButton').addEventListener('click', () => scrollCards_V('prev'));
+
 displayVelas();
 //______________Piedras_______________________________________________________________________________________________________________________________________
 
+import { piedras } from './Productos.js';
+console.log(piedras);
+
+function displayPiedras() {
+  const p_container = document.getElementById('piedras-container');
+
+  piedras.forEach(product => {
+      const p_card = `
+          <div class="card">
+              <img src="${product.image}" class="card-img-top" alt="${product.name}">
+              <div class="card-body">
+                  <h5 class="card-title">${product.name}</h5>
+                  <p class="card-text">${product.description}</p>
+              </div>
+          </div>
+      `;
+      p_container.innerHTML += p_card;
+  });
+}
+
+function scrollCards_P(direction) {
+  const p_container = document.getElementById('piedras-container');
+  const cardWidth = 2 * p_container.children[0].offsetWidth + 20; 
+
+  if (direction === 'next') {
+      p_container.scrollBy({ left: cardWidth, behavior: 'smooth' });
+  } else {
+      p_container.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+  }
+}
+
+document.getElementById('p_nextButton').addEventListener('click', () => scrollCards_P('next'));
+document.getElementById('p_prevButton').addEventListener('click', () => scrollCards_P('prev'));
+
+displayPiedras();
 
 //_____________________________________________________________________________________________________________________________________________________
